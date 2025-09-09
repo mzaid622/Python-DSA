06b46000d5f4587e187b01e531d7e4b05ec13c57
mzaid622 <m.zaid990447@gmail.com>
2025-09-09 16:40:08 +0500

    Stock buy Sell 2nd approach

--- Arrays/Best_Time_to_Buy_and_Sell_Stock.py
+++ Arrays/Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,10 +1,20 @@
 prices = [7,1,5,3,6,4]
 
-profit = 0
-length = len(prices)
-for i in range(length):
-  for j in range(length):
-    if (j > i):
-      profit = max(profit,prices[j] - prices[i])
+# This is brute Force
+# profit = 0
+# length = len(prices)
+# for i in range(length):
+#   for j in range(length):
+#     if (j > i):
+#       profit = max(profit,prices[j] - prices[i])
 
-print(profit)
+# print(profit)
+
+
+max_price = 0
+min_price = float('inf')
+
+for i in prices:
+  min_price = min(min_price,i)
+  max_price = max(max_price,i-min_price)
+print(max_price)
