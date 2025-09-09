323952d56a9963fb6ebda9ff4f903206858a5321
mzaid622 <m.zaid990447@gmail.com>
2025-09-09 14:32:24 +0500

    Majority Element

---
+++ Arrays/Majority_Element.py
@@ -0,0 +1,20 @@
+# This is solving Hash Map
+# nums = [2,2,1,1,1,2,2]
+# dict = {
+# }
+# for num in nums:
+#   if num in dict:
+#     dict[num] += 1
+#   else:
+#     dict[num] =1
+# answer = 0
+# for key,value in dict.items():
+#   if (len(nums)//2 < value):
+#     answer = key
+#     break
+# print(answer)    
+
+
+# 2nd Approach Boyer-Moore Voting Algo
+
+nums = [2,2,1,1,1,2,2]
