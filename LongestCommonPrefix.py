04f31247b61d2d10917d21e2a8a3d49221288a94
mzaid622 <m.zaid990447@gmail.com>
2025-09-10 16:49:17 +0500

    Longest Common Prefix 2

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -1,17 +1,23 @@
 strs = ["flower", "flow", "flight"]
 
-length = len(strs[0])
-result = ""
-for i in range(length):
-  char = strs[0][i]
-  for j in range(1,len(strs)):
-    if (char != strs[j][i]):
-      print(result)
-      exit()
-  result += char
-  
-
-
-      
-print(result)
+# length = len(strs[0])
+# result = ""
+# for i in range(length):
+#   char = strs[0][i]
+#   for j in range(1,len(strs)):
+#     if (char != strs[j][i]):
+#       print(result)
+#       exit()
+#   result += char
+# print(result)
     
+# 2nd Approach
+new_list = sorted(strs)
+min_length = min(len(new_list[0]),len(new_list[-1]))
+result = ""
+for i in range(min_length):
+  if new_list[0][i] == new_list[-1][i]:
+    result += new_list[0][i]
+  else:
+    break
+print(result)
