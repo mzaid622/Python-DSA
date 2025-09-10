26d96cd2218f394b0dde17f01725ad06697ffe9b
mzaid622 <m.zaid990447@gmail.com>
2025-09-10 15:43:07 +0500

    Length of Last Word optimiza approach

--- Arrays/Length_of_Last_Word.py
+++ Arrays/Length_of_Last_Word.py
@@ -1,7 +1,20 @@
-s = "Hello World"
-new_s=s.split()
+# s = "Hello World"
+# new_s=s.split()
 
-last_word = new_s[-1]
+# last_word = new_s[-1]
 
-print(len(last_word))
+# print(last_word)
+
+# 2nd Approach
+
+str = "fly me   to   the moon  "
+length=len(str)
+count = 0
+for i in range(length-1,-1,-1):
+  if (str[i] == " "):
+    if count > 0:
+      break
+  else:
+    count +=1
+print(count)
 
