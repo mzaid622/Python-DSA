aabb6587f1847ba8dfcce853b44c8ea91d464c70
mzaid622 <m.zaid990447@gmail.com>
2025-09-15 18:21:14 +0500

    2nd time

--- Arrays/Reverse_Words_in_str.py
+++ Arrays/Reverse_Words_in_str.py
@@ -3,36 +3,32 @@ s = "the sky is blue"
 
 # Approach 1
 
-# new_s=s.split()
-# length = len(new_s) -1
-# answer = ""
-# for i in range(length,-1,-1):
-#   answer += new_s[i]
-#   answer += " "
-# print(answer.strip())
+new_s=s.split()
+length = len(new_s) -1
+answer = ""
+for i in range(length,-1,-1):
+  answer += new_s[i]
+  answer += " "
+print(answer.strip())
 
 
+# Approach 2
+# i = len(s) - 1
+# answer = []
+# while i >= 0:
+#     while i >= 0 and s[i] == " ":
+#         i -= 1
 
-# Approach 2 
-i = len(s) - 1
-answer = []
-while i >= 0:
-    while i >= 0 and s[i] == " ":
-        i -= 1
+#     if i < 0:
+#         break
 
-    if i < 0:
-        break
-    
-    end = i
-    while i>=0 and s[i] != " ":
-        i -=1
+#     end = i
+#     while i >= 0 and s[i] != " ":
+#         i -= 1
 
-    start = i+1
+#     start = i + 1
 
-    answer.append(s[start:end+1])
-
-    i = start - 1
-
-print(answer)
+#     answer.append(s[start : end + 1])
 
 
+# print(answer)
